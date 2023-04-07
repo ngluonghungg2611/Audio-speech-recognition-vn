865f2344943824afebeed1c1f16865426ab34e7a
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2023-04-07 21:32:06 +0700

    record audio

---
+++ record_audio.py
@@ -0,0 +1,40 @@
+import pyaudio
+import wave
+from tqdm import tqdm
+# thiết lập thông số âm thanh
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+CHUNK = 1024
+RECORD_SECONDS = 10
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+# khởi tạo PyAudio
+audio = pyaudio.PyAudio()
+
+# bắt đầu ghi âm
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+
+print("Đang ghi âm...")
+frames = []
+
+for i in tqdm(range(0, int(RATE / CHUNK * RECORD_SECONDS)), desc="Recording..."):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("Đã ghi âm xong!")
+
+# kết thúc ghi âm
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+# lưu tệp âm thanh
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(audio.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
